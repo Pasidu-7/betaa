6521c81453f3d0913f49900e6718cb7e0d9e017d
Pasidu Hettarachchi <phettarachchi@mitrai.local>
2021-01-14 15:04:50 +0530

    Changes made of backups

--- touch.sh
+++ touch.sh
@@ -10,12 +10,12 @@ git add .
 
 git commit -m "Changes made of backups"
 
-git push https://github.com/Pasidu-7/betaa.git
 
-#git remote add origin https://github.com/Pasidu-7/betaa.git
 
-#git remote -v
+git remote add origin https://github.com/Pasidu-7/betaa.git
 
-#git put -u origin master 
+git remote -v
+
+git push -u origin master 
 
 
